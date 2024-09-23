b214ae4e8741a229601b20b62e89839174aa8156
Stef_DP <stefano.delprete.08@gmail.com>
2024-09-24 01:18:03 +0200

    Add files via upload

---
+++ media.js
@@ -0,0 +1,250 @@
+const fs = require("node:fs");
+const path = require("node:path");
+const { execSync } = require("node:child_process");
+
+const player = process.env.PLAYER || null;
+
+debugLog(`Using player: ${player || 'default'}`)
+
+const template = JSON.stringify({
+	text: "{{text}}",
+	alt: "{{icon}}",
+	class: "{{class}}",
+	tooltip: "{{tooltip}}",
+});
+
+const noLyrics = template
+	.replace("{{text}}", "No Lyrics Avaible")
+	.replace("{{icon}}", "none")
+	.replace('{{class}}', 'none')
+	.replace('{{tooltip}}', 'none');
+
+const noSong = template
+	.replace("{{text}}", "No Song Playing")
+	.replace("{{icon}}", "none")
+	.replace('{{class}}', 'none')
+	.replace('{{tooltip}}', 'none');
+
+const lyricsCacheFile = path.join(
+	process.env.HOME,
+	".cache",
+	"custom_commands",
+	"lyrics.json",
+);
+
+let currentSeconds
+let rawMetadata
+let volume
+
+try {
+    const currentTime = execSync(`playerctl ${player ? `-p ${player}` : ''} position`).toString().trim();
+    
+    currentSeconds = Math.round(Number.parseFloat(currentTime) + 1);
+
+	debugLog(`currentSeconds: ${currentSeconds}`)
+
+    rawMetadata = execSync(
+        `playerctl metadata ${player ? `-p ${player}` : ''} --format "{{artist}}|{{album}}|{{title}}|{{mpris:trackid}}"`,
+    )
+        .toString()
+        .trim()
+        .split("|");
+
+	debugLog(`rawMetadata: ${rawMetadata}`)
+
+	volume = execSync(
+		`playerctl ${player ? `-p ${player}` : ''} volume`
+	)
+
+	debugLog(`volume: ${volume}`)
+} catch(e) {
+	debugLog('Something went wrong while getting data from playerctl', e)
+
+    if (process.argv.includes("--data") || process.argv.includes("-d")) {
+        console.log(noSong)
+        
+        process.exit(0)
+    }
+
+    console.log(noLyrics)
+
+    process.exit(0)
+}
+
+const parsedMetadata = {
+	artist: rawMetadata[0],
+	album: rawMetadata[1],
+	track: rawMetadata[2],
+	trackId: rawMetadata[3],
+};
+
+debugLog(`parsedMetadata: ${JSON.stringify(parsedMetadata, null, 4)}`)
+
+if (process.argv.includes("--data") || process.argv.includes("-d")) {
+	if (!parsedMetadata.track) {
+		debugLog("Metadata doesn't include the song name")
+		console.log(noSong);
+
+		process.exit(0);
+	}
+
+	volume = Number.parseInt(Number.parseFloat(volume) * 100)
+
+	const currentSong = template
+		.replace("{{text}}", `${parsedMetadata.artist} - ${parsedMetadata.track}`)
+		.replace("{{icon}}", "playing")
+		.replace('{{class}}', 'none')
+		.replace('{{tooltip}}', `Volume: ${volume}%`);
+
+	debugLog("Current Song Data")
+	console.log(currentSong);
+
+	process.exit(0);
+}
+
+(async () => {
+	const lyrics = await getLyrics(parsedMetadata);
+
+	let firstLyric;
+	let lastLyric;
+
+	const lyricsSplit = lyrics
+		.split("\n")
+		.map((lyric) => {
+			let lyricText = lyric.split(" ");
+			const time = lyricText.shift().replace(/[\[\]]/g, "");
+
+			lyricText = lyricText.join(" ");
+
+			if (lyricText.length > 0) return [time, lyricText];
+		})
+		.filter(Boolean);
+
+	for (const lyric of lyricsSplit) {
+		const timestamp = lyric[0];
+		const text = lyric[1];
+
+		if (!firstLyric) firstLyric = text;
+
+		const minutes = timestamp.split(":")[0];
+		const seconds = timestamp.split(":")[1];
+
+		const totalSeconds =
+			Number.parseFloat(minutes) * 60 + Number.parseFloat(seconds);
+
+		if (currentSeconds >= totalSeconds) lastLyric = text;
+	}
+
+	if (!lastLyric && !firstLyric) {
+		debugLog("No lastLyric and firstLyric avaible")
+		console.log(noLyrics);
+
+		process.exit(0);
+	}
+
+	const output = template
+		.replace("{{text}}", lastLyric || firstLyric)
+		.replace("{{icon}}", "lyrics")
+		.replace('{{class}}', 'none')
+		.replace('{{tooltip}}', 'none');
+
+	debugLog("Current Song Synced Lyric")
+	console.log(output);
+	process.exit(0);
+})();
+
+async function fetchLyrics(metadata) {
+	debugLog(`Fetching the lyrics for "${metadata.track}" from "${metadata.album}" from "${metadata.artist}" (${metadata.trackId})`)
+	
+	const cacheData = {
+		trackId: metadata.trackId,
+		lyrics: null,
+	};
+
+	const searchParams = [
+		`track_name=${encodeURIComponent(metadata.track)}`,
+		`artist_name=${encodeURIComponent(metadata.artist)}`,
+		`album_name=${encodeURIComponent(metadata.album)}`,
+		`q=${encodeURIComponent(metadata.track)}`,
+	];
+
+	const url = `https://lrclib.net/api/search?${searchParams.join("&")}`;
+
+	const res = await fetch(url);
+
+	if (!res.ok) {
+		debugLog(`Lyrics fetch request failed with status ${res.status} (${res.statusText})`)
+		
+		console.log(noLyrics);
+
+		fs.writeFileSync(lyricsCacheFile, JSON.stringify(cacheData, null, 4));
+
+		process.exit(0);
+	}
+
+	const data = await res.json();
+
+	const match = data.find(
+		(d) => d.artistName === metadata.artist && d.trackName === metadata.track,
+	);
+
+	if (!match || !match.syncedLyrics || match.syncedLyrics?.length <= 0) {
+		debugLog("The fetched song does not have synced lyrics")
+		console.log(noLyrics);
+
+		fs.writeFileSync(lyricsCacheFile, JSON.stringify(cacheData, null, 4));
+
+		process.exit(0);
+	}
+
+	debugLog("Successfully fetched and cached the synced lyrics")
+
+	cacheData.lyrics = match.syncedLyrics;
+
+	fs.writeFileSync(lyricsCacheFile, JSON.stringify(cacheData, null, 4));
+
+	return match.syncedLyrics;
+}
+
+async function getLyrics(metadata) {
+	let cachedLyrics;
+
+	if (fs.existsSync(lyricsCacheFile)) {
+		const lyricsCache = fs.readFileSync(lyricsCacheFile, "utf-8");
+
+		try {
+			const cachedLyricsData = JSON.parse(lyricsCache);
+
+			if (metadata.trackId === cachedLyricsData.trackId) {
+				if (!cachedLyricsData.lyrics) {
+					debugLog("Cached lyrics are null")
+					console.log(noLyrics);
+
+					process.exit(0);
+				}
+
+				debugLog("Loaded lyrics from file")
+
+				cachedLyrics = cachedLyricsData.lyrics;
+			} else {
+				debugLog("Cached song is different from current song, fetching the song data")
+
+				cachedLyrics = await fetchLyrics(metadata);
+			}
+		} catch (e) {
+			debugLog("Cached file content is not a valid JSON, fetching the song data")
+
+			cachedLyrics = await fetchLyrics(metadata);
+		}
+	} else {
+		debugLog("Cache file doesn't exist, fetching the song data")
+
+		cachedLyrics = await fetchLyrics(metadata);
+	}
+
+	return cachedLyrics;
+}
+
+function debugLog(...args) {
+	if (process.env.DEBUG === "true") console.log('\x1b[35;1mDEBUG:\x1b[0m', ...args)
+}
