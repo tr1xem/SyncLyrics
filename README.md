6a200d405bc21e99447a5c7872ffe7de208f6851
Stef-00012 <stefano.delprete.08@gmail.com>
2024-09-27 19:43:06 +0200

    added customizable cache folder, lyrics now have a tooltip, added flags to save the lyrics and get onlt the song or artist name, escaped html from lyrics

--- README.md
+++ README.md
@@ -17,21 +17,128 @@ Just clone this repository
 `node media.js` (by default will use first player returned by `playerctl`).
 
 - You can specify your player by running it as `PLALYER=<name> node media.js` (Example for Spotify: `PLAYER=spotify node media.js`).
+- You can change the current lyric color in the tooltip by running it as `TOOLTIP_CURRENT_LYRIC_COLOR=#abcdef node media.js`.
 - You can see debug logs by running `DEBUG=true node media.js`.
-- You can change the config folder by running it as `LYRICS_FOLDER=/path/to/folder node media.js`.
+- You can change the cache folder by running it as `CACHE_FOLDER=/path/to/folder node media.js`.
+- You can change the config folder by running it as `CONFIG_FOLDER=/path/to/folder node media.js`.
+
+*(All of those can be ran together too).*
 
 ### Flags
 
-- `--data`, `-d`: Returns the song name and artist instead of the lyrics
-- `--trackid`, `-tid`: Returns the song ID (required for local lyrics)
+- `--data`, `-d`: Returns the song name and artist.
+- `--trackid`, `-tid`: Returns the song ID (required for local lyrics).
+- `--name`, `-n`: Returns the song name.
+- `--artist`, `-a`: Returns the artist name.
+- `--show-lyrics`, `-sl`: Saves the lyrics in a temporary file (`/tmp/lyrics.txt`) and opens the file (when combined with `--save` or `-s` it saves the lyrics in a permanent file, `~/Downloads/syncLyrics/<song_name>-<artist_name>.txt`).
+
+## Waybar Example
+
+This example uses `spotify` as player and has the `media.js` file located in `~/.config/custom-commands/media.js`
+
+```json
+{
+    "custom/song": {
+		"tooltip": true,
+		"format": "{icon} {}",
+		"format-icons": {
+			"playing": "󰎇 ",
+			"none": "󰎊 "
+		},
+		"return-type": "json",
+		"exec-if": "if [ -f ~/.config/custom-commands/media.js ]; then exit 0; else exit 1; fi",
+		"interval": 1,
+		"exec": "PLAYER=spotify node ~/.config/custom-commands/media.js --data",
+		"on-click": "playerctl -p spotify play-pause",
+		"on-click-middle": "pgrep -x 'spotify' > /dev/null && wmctrl -a 'Spotify' || spotify &",
+		"on-scroll-up": "playerctl -p spotify volume 0.01+",
+		"on-scroll-down": "playerctl -p spotify volume 0.01-",
+		"escape": true,
+		"exec-on-event": false
+	},
+
+	"custom/lyrics": {
+		"tooltip": true,
+		"format": "{icon} {}",
+		"format-icons": {
+			"lyrics": "󰲹 ",
+			"none": "󰐓 "
+		},
+		"return-type": "json",
+		"exec-if": "if [ -f ~/.config/custom-commands/media.js ]; then exit 0; else exit 1; fi",
+		"interval": 1,
+		"exec": "PLAYER=spotify node ~/.config/custom-commands/media.js",
+		"on-click-middle": "PLAYER=spotify node ~/.config/custom-commands/media.js --show-lyrics",
+		"escape": true,
+		"exec-on-event": false
+	}
+}
+```
+
+### Song Name Progress
+
+You can show the progress bar in the `custom/song` module by adding this CSS to your waybar's CSS:
+
+```css
+#custom-song.perc0-0 {
+    background-image: linear-gradient(
+    to right,
+    #a6e3a1 0.0%,
+    #80c47a 0.1%
+    );
+}
+
+#custom-song.perc1-0 {
+    background-image: linear-gradient(
+    to right,
+    #a6e3a1 1.0%,
+    #80c47a 1.1%
+    );
+}
+
+#custom-song.perc2-0 {
+    background-image: linear-gradient(
+    to right,
+    #a6e3a1 2.0%,
+    #80c47a 2.1%
+    );
+}
+
+/* 
+    Do the same with all the classes from #custom-song.perc3-0 to #custom-song.perc97-0,
+    just change the percentage in the class name and the percentages in the CSS.
+    */
+
+#custom-song.perc98-0 {
+    background-image: linear-gradient(
+    to right,
+    #a6e3a1 98.0%,
+    #80c47a 98.1%
+    );
+}
+
+#custom-song.perc99-0 {
+    background-image: linear-gradient(
+    to right,
+    #a6e3a1 99.0%,
+    #80c47a 99.1%
+    );
+}
+
+#custom-song.perc100-0 {
+    background-image: linear-gradient(
+    to right,
+    #a6e3a1 100.0%,
+    #80c47a 100.1%
+    );
+}
+```
 
 ## Local Lyrics
 
 You can add your own lyrics for it to use, if you add a custom lyrics file, it will be preferred over the API.
 
-default lyrics folder is `~/.config/syncLyrics` (create if it doesn't exist), the config folder can be changed with the `LYRICS_FOLDER` env.
-
-the files in this folder must be named `<track_id>.txt` (Example: `5nAu0J2rlijocTGX8QWo07.txt`) and their content must be formatted as `[mm:ss.xx] <lyrics here>` and each one must be on a new line.
+The lyrics are read from the `$CONFIG_FOLDER/lyrics` folder, the files in this folder must be named `<track_id>.txt` (Example: `5nAu0J2rlijocTGX8QWo07.txt`) and their content must be formatted as `[mm:ss.xx] <lyrics here>` and each one must be on a new line.
 
 Example:
 ```txt
--- media.js
+++ media.js
@@ -3,10 +3,26 @@ const path = require("node:path");
 const { execSync } = require("node:child_process");
 
 const player = process.env.PLAYER || null;
-const lyricsFolder =
-	process.env.LYRICS_FOLDER ||
+
+const configFolder =
+	process.env.CONFIG_FOLDER ||
 	path.join(process.env.HOME, ".config", "syncLyrics");
 
+const lyricsCacheDir =
+	process.env.CACHE_FOLDER ||
+	path.join(process.env.HOME, ".cache", "syncLyrics");
+
+const tooltipCurrentLyricColor =
+	process.env.TOOLTIP_CURRENT_LYRIC_COLOR || "#cba6f7";
+
+if (!fs.existsSync(lyricsCacheDir)) {
+	fs.mkdirSync(lyricsCacheDir, {
+		recursive: true,
+	});
+}
+
+debugLog(`Using config folder: ${configFolder}`);
+debugLog(`Using cache folder: ${lyricsCacheDir}`);
 debugLog(`Using player: ${player || "default"}`);
 
 const template = JSON.stringify({
@@ -28,10 +44,6 @@ const noSong = template
 	.replace("{{class}}", "none")
 	.replace("{{tooltip}}", "none");
 
-const lyricsCacheDir = path.join(process.env.HOME, ".cache", "syncLyrics");
-
-const lyricsCacheFile = path.join(lyricsCacheDir, "lyrics.json");
-
 let currentSeconds;
 let rawMetadata;
 let volume;
@@ -48,13 +60,13 @@ try {
 	debugLog(`currentSeconds: ${currentSeconds}`);
 
 	rawMetadata = execSync(
-		`playerctl metadata ${player ? `-p ${player}` : ""} --format "{{artist}}|{{album}}|{{title}}|{{mpris:trackid}}"`,
+		`playerctl metadata ${player ? `-p ${player}` : ""} --format "{{artist}}|{{album}}|{{title}}|{{mpris:trackid}}|{{mpris:length}}"`,
 	)
 		.toString()
 		.trim()
 		.split("|");
 
-	debugLog(`rawMetadata: ${rawMetadata.join("|")}`);
+	debugLog(`rawMetadata: ${rawMetadata}`);
 
 	volume = execSync(`playerctl ${player ? `-p ${player}` : ""} volume`);
 
@@ -78,53 +90,151 @@ const parsedMetadata = {
 	album: rawMetadata[1],
 	track: rawMetadata[2],
 	trackId: rawMetadata[3],
+	microSecondsLength: rawMetadata[4],
 };
 
 debugLog(`parsedMetadata: ${JSON.stringify(parsedMetadata, null, 4)}`);
 
-if (process.argv.includes("--data") || process.argv.includes("-d")) {
-	if (!parsedMetadata.track) {
-		debugLog("Metadata doesn't include the song name");
-		console.log(noSong);
+const lyricsCacheFile = path.join(lyricsCacheDir, "lyrics.json");
+
+(async () => {
+	if (process.argv.includes("--data") || process.argv.includes("-d")) {
+		const currentMs = currentSeconds * 1000;
+		const totalMs = parsedMetadata.microSecondsLength / 1000;
+
+		const percentage = Math.round((currentMs / totalMs) * 100);
+
+		if (!parsedMetadata.track) {
+			debugLog("Metadata doesn't include the song name");
+			console.log(noSong);
+
+			process.exit(0);
+		}
+
+		volume = Number.parseInt(Number.parseFloat(volume) * 100);
+
+		const currentSong = template
+			.replace("{{text}}", `${parsedMetadata.artist} - ${parsedMetadata.track}`)
+			.replace("{{icon}}", "playing")
+			.replace("{{class}}", `perc${percentage}-0`)
+			.replace("{{tooltip}}", `Volume: ${volume}%`);
+
+		debugLog("Current Song Data");
+		console.log(currentSong);
 
 		process.exit(0);
 	}
 
-	volume = Number.parseInt(Number.parseFloat(volume) * 100);
+	if (process.argv.includes("--trackid") || process.argv.includes("-tid")) {
+		const trackId = parsedMetadata.trackId.split("/").pop();
 
-	const currentSong = template
-		.replace("{{text}}", `${parsedMetadata.artist} - ${parsedMetadata.track}`)
-		.replace("{{icon}}", "playing")
-		.replace("{{class}}", "none")
-		.replace("{{tooltip}}", `Volume: ${volume}%`);
+		console.log(`Your track ID is ${trackId}`);
 
-	debugLog("Current Song Data");
-	console.log(currentSong);
+		process.exit(0);
+	}
 
-	process.exit(0);
-}
+	if (process.argv.includes("--name") || process.argv.includes("-n")) {
+		const currentMs = currentSeconds * 1000;
+		const totalMs = parsedMetadata.microSecondsLength / 1000;
 
-if (process.argv.includes("--trackid") || process.argv.includes("-tid")) {
-	const trackId = parsedMetadata.trackId.split("/").pop();
+		const percentage = Math.round((currentMs / totalMs) * 100);
 
-	console.log(`Your track ID is ${trackId}`);
+		const trackName = parsedMetadata.track;
 
-	process.exit(0);
-}
+		if (!trackName) {
+			debugLog("Metadata doesn't include the song name");
+			console.log(noSong);
 
-(async () => {
+			process.exit(0);
+		}
+
+		const currentSongName = template
+			.replace("{{text}}", trackName)
+			.replace("{{icon}}", "track")
+			.replace("{{class}}", `perc${percentage}-0`)
+			.replace("{{tooltip}}", "none");
+
+		console.log(currentSongName);
+
+		process.exit(0);
+	}
+
+	if (process.argv.includes("--artist") || process.argv.includes("-a")) {
+		const currentMs = currentSeconds * 1000;
+		const totalMs = parsedMetadata.microSecondsLength / 1000;
+
+		const percentage = Math.round((currentMs / totalMs) * 100);
+
+		const artistName = parsedMetadata.artist;
+
+		if (!artistName) {
+			debugLog("Metadata doesn't include the artist name");
+			console.log(noSong);
+
+			process.exit(0);
+		}
+
+		const currentArtist = template
+			.replace("{{text}}", artistName)
+			.replace("{{icon}}", "artist")
+			.replace("{{class}}", `perc${percentage}-0`)
+			.replace("{{tooltip}}", "none");
+
+		console.log(currentArtist);
+
+		process.exit(0);
+	}
+
 	const lyrics = await getLyrics(parsedMetadata);
+
+	if (process.argv.includes("--show-lyrics") || process.argv.includes("-sl")) {
+		if (process.argv.includes("--save") || process.argv.includes("-s")) {
+			const downloadFolder = path.join(
+				process.env.HOME,
+				"Downloads",
+				"syncLyric",
+			);
+
+			if (!fs.existsSync(downloadFolder)) fs.mkdirSync(downloadFolder);
+
+			const fileName = `${parsedMetadata.track.replaceAll(" ", "_")}-${parsedMetadata.artist.replaceAll(" ", "_")}.txt`;
+
+			const filePath = path.join(downloadFolder, fileName);
+
+			fs.writeFileSync(filePath, lyrics.replaceAll("\\n", "\n"));
+
+			execSync(`xdg-open ${filePath}`);
+
+			process.exit(0);
+		}
+
+		const tmpFilePath = path.join("/", "tmp", "lyrics.txt");
+
+		fs.writeFileSync(tmpFilePath, lyrics.replaceAll("\\n", "\n"));
+
+		execSync(`xdg-open ${tmpFilePath}`);
+
+		process.exit(0);
+	}
 
 	let firstLyric;
 	let lastLyric;
+
+	let firstTimestamp;
+	let lastTimestamp;
 
 	const lyricsSplit = lyrics
 		.split("\n")
 		.map((lyric) => {
 			let lyricText = lyric.split(" ");
+
 			const time = lyricText.shift().replace(/[\[\]]/g, "");
 
-			lyricText = lyricText.join(" ");
+			lyricText = lyricText
+				.join(" ")
+				.replaceAll('"', '\\"')
+				.replaceAll("<", "&lt;")
+				.replaceAll(">", "&gt;");
 
 			if (lyricText.length > 0) return [time, lyricText];
 		})
@@ -135,6 +245,7 @@ if (process.argv.includes("--trackid") || process.argv.includes("-tid")) {
 		const text = lyric[1];
 
 		if (!firstLyric) firstLyric = text;
+		if (!firstTimestamp) firstTimestamp = timestamp;
 
 		const minutes = timestamp.split(":")[0];
 		const seconds = timestamp.split(":")[1];
@@ -142,21 +253,56 @@ if (process.argv.includes("--trackid") || process.argv.includes("-tid")) {
 		const totalSeconds =
 			Number.parseFloat(minutes) * 60 + Number.parseFloat(seconds);
 
-		if (currentSeconds >= totalSeconds) lastLyric = text;
+		if (currentSeconds >= totalSeconds) {
+			lastLyric = text;
+			lastTimestamp = timestamp;
+		}
 	}
 
-	if (!lastLyric && !firstLyric) {
+	const searchLyric = lastLyric || firstLyric;
+	const searchTimestamp = lastTimestamp || firstTimestamp;
+
+	if (!searchLyric) {
 		debugLog("No lastLyric and firstLyric avaible");
 		console.log(noLyrics);
 
 		process.exit(0);
 	}
 
+	let previousLinesAmount = 0;
+	let nextLinesAmount = 0;
+
+	const currentLyricIndex = lyricsSplit.findIndex(
+		(lyric) => lyric[0] === searchTimestamp && lyric[1] === searchLyric,
+	);
+
+	if (currentLyricIndex === 1) previousLinesAmount = 1;
+	else if (currentLyricIndex === 2) previousLinesAmount = 2;
+	else if (currentLyricIndex >= 3) previousLinesAmount = 3;
+
+	if (currentLyricIndex === lyricsSplit.length - 1) nextLinesAmount = 1;
+	else if (currentLyricIndex === lyricsSplit.length - 2) nextLinesAmount = 2;
+	else if (currentLyricIndex <= lyricsSplit.length - 3) nextLinesAmount = 3;
+
+	const previousLines = [...lyricsSplit]
+		.splice(currentLyricIndex - previousLinesAmount, previousLinesAmount)
+		.map((lyric) => lyric[1]);
+
+	const nextLines = [...lyricsSplit]
+		.splice(currentLyricIndex + 1, nextLinesAmount)
+		.map((lyric) => lyric[1]);
+
+	const tooltip = `${previousLines.join("\\n")}${
+		previousLines.length > 0 ? "\\n" : ""
+	}<span color=\\"${tooltipCurrentLyricColor}\\"><i>${searchLyric}</i></span>${
+		nextLines.length > 0 ? "\\n" : ""
+	}${nextLines.join("\\n")}`;
+
 	const output = template
-		.replace("{{text}}", lastLyric || firstLyric)
+		.replace("{{text}}", searchLyric)
 		.replace("{{icon}}", "lyrics")
 		.replace("{{class}}", "none")
-		.replace("{{tooltip}}", "none");
+		.replace("{{tooltip}}", tooltip);
 
 	debugLog("Current Song Synced Lyric");
 	console.log(output);
@@ -172,11 +318,6 @@ async function fetchLyrics(metadata) {
 		trackId: metadata.trackId,
 		lyrics: null,
 	};
-
-	if (!fs.existsSync(lyricsCacheDir))
-		fs.mkdirSync(lyricsCacheDir, {
-			recursive: true,
-		});
 
 	const searchParams = [
 		`track_name=${encodeURIComponent(metadata.track)}`,
@@ -240,7 +381,7 @@ async function getLyrics(metadata) {
 
 	const trackId = metadata.trackId.split("/").pop();
 
-	const localLyricsFile = path.join(lyricsFolder, `${trackId}.txt`);
+	const localLyricsFile = path.join(configFolder, "lyrics", `${trackId}.txt`);
 
 	if (fs.existsSync(localLyricsFile)) {
 		debugLog("Loading lyrics from local file");
